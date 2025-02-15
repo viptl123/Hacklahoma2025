794f26b40caddc93e79ca0560950a66ec6594343
Vishnu Patel <viptl123@gmail.com>
2025-02-15 17:50:56 -0600

    Revert "Revert "fskd""
    This reverts commit d9c8bd44543e31e7a826dae4685eebea3eb2ef1d.

---
+++ StepScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepScreenApp.swift
+//  StepScreen
+//
+//  Created by Arman Riaz on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct StepScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
